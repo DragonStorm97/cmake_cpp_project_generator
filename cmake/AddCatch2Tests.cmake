72c50ee7e576bc77aa5fd2f8f20fefbbc281a4de
Johnathan Jacobs <jonathan.jacobs1997@gmail.com>
2024-06-25 20:21:13 +0200

    add a relaxed_constexpr executable for tests, fix hardcoded project name

--- cmake/AddCatch2Tests.cmake
+++ cmake/AddCatch2Tests.cmake
@@ -2,7 +2,7 @@
 
 function(add_catch2_tests app_name is_lib)
   if({{REPLACE_ME_PROJECT_NAME}}_ENABLE_CATCH2_TESTS)
-    set(test_name ${app_name}_tests)
+    set(app_test_name ${app_name}_tests)
     if({{REPLACE_ME_PROJECT_NAME}}_ENABLE_GLOBS)
       file(
         GLOB_RECURSE
@@ -10,28 +10,34 @@ function(add_catch2_tests app_name is_lib)
         CONFIGURE_DEPENDS
         "*.cpp")
     else()
-      set(sources "${test_name}.cpp")
+      set(sources "${app_test_name}.cpp")
     endif()
 
-    add_executable(${test_name} ${sources})
+    # build a second version of the test with STATIC_REQUIRE as REQUIRE to debug constexpr test failures
+    set(test_names relaxed_constexpr_${app_test_name} ${app_test_name})
+    foreach(test_name IN LIST test_names)
+      add_executable(${test_name} ${sources})
 
-    set_target_properties(${test_name} PROPERTIES CXX_EXTENSIONS OFF)
-    target_compile_features(${test_name} PUBLIC cxx_std_{{REPLACE_ME_STD_VERSION}})
-    target_compile_options(${test_name} PRIVATE ${DEFAULT_COMPILER_OPTIONS_AND_WARNINGS})
+      set_target_properties(${test_name} PROPERTIES CXX_EXTENSIONS OFF)
+      target_compile_features(${test_name} PUBLIC cxx_std_{{REPLACE_ME_STD_VERSION}})
+      target_compile_options(${test_name} PRIVATE ${DEFAULT_COMPILER_OPTIONS_AND_WARNINGS})
 
-    # Add library as dependency
-    target_include_directories(${test_name} PUBLIC ${PROJECT_SOURCE_DIR}/include/${CMAKE_PROJECT_NAME})
-    target_link_libraries(${test_name} PRIVATE {{REPLACE_ME_LIB_NAME}})
-    target_include_directories(${test_name} PUBLIC ${PROJECT_SOURCE_DIR}/${CMAKE_PROJECT_NAME}/library/include)
+      # Add library as dependency
+      target_include_directories(${test_name} PUBLIC ${PROJECT_SOURCE_DIR}/include/${CMAKE_PROJECT_NAME})
+      target_include_directories(${test_name} PUBLIC ${PROJECT_SOURCE_DIR}/${CMAKE_PROJECT_NAME}/library/include)
 
-    # add the app as a dependency if it's not the library
-    if(NOT IS_LIB)
-      target_include_directories(${test_name} PUBLIC ${PROJECT_SOURCE_DIR}/${CMAKE_PROJECT_NAME}/apps/${app_name}/include)
-    endif()
+      # add the app as a dependency if it's not the library
+      if(NOT IS_LIB)
+        target_include_directories(${test_name} PUBLIC ${PROJECT_SOURCE_DIR}/${CMAKE_PROJECT_NAME}/apps/${app_name}/include)
+      endif()
 
-    target_link_libraries(${test_name} PRIVATE Catch2::Catch2WithMain)
+      target_link_libraries(${test_name} PRIVATE {{REPLACE_ME_LIB_NAME}} Catch2::Catch2WithMain)
 
-    catch_discover_tests(${test_name} LABEL ${app_name})
+      catch_discover_tests(${test_name} LABEL ${app_name})
 
+      if(${test_name} MATCHES "^relaxed.*")
+        target_compile_definitions(${test_name} PRIVATE -DCATCH_CONFIG_RUNTIME_STATIC_REQUIRE)
+      endif()
+    endforeach()
   endif()
 endfunction()
